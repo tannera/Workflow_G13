4d7f5d3bda8085c4afb888669723cf0307550c9b
Diego Williams Aguilar Montaño <diegoaguilar.88@gmail.com>
2015-05-31 20:21:11 +0200

    Adding Wiretap

--- src/main/java/org/group13/routingLogic/DataLogging.java
+++ src/main/java/org/group13/routingLogic/DataLogging.java
@@ -11,22 +11,33 @@ import org.group13.transformerBeans.ConvertToOrderBean;
 public class DataLogging extends RouteBuilder{
 	public void configure() {
 		// used Wiretap, which created a logging copy
-		// add some custom logic for storing
-		
-		/*from("jms:InvoicesTap")
+		// add some custom logic for storing				
+		// and then put in jdbc:ProcurementDB queue
+		// which is configured in MainApp as datasource in registry
+		// so basically the object is stored in DB
+		from("jms:InvoicesTap")
 	    .process(new Processor() {
 	            public void process(Exchange exchange) throws Exception {
-	           	 Invoice invoice = exchange.getIn().getBody(Invoice.class);
-	           	 System.out.println("Wiretaped invoice quantity "+invoice.getItemQuantity());
+	           	Invoice invoice = exchange.getIn().getBody(Invoice.class);
+	           	exchange.getIn().setBody("INSERT INTO Invoices_log (company,description,quantity) "
+	           			+ "VALUES('" + invoice.getCustomer() + "','"
+	           					+ invoice.getItemDescription() +"','"
+	           					+ invoice.getItemQuantity() + "');");
+	            
 	            }
-	        });
+	            })
+	        .to("jdbc:ProcurementDB");
 		
 		from("jms:OrdersTap")
 	    .process(new Processor() {
 	            public void process(Exchange exchange) throws Exception {
-	           	 Order order = exchange.getIn().getBody(Order.class);
-	           	 System.out.println("Wiretaped order quantity "+order.getItemQuantity());
+	           	Order order = exchange.getIn().getBody(Order.class);
+	           	exchange.getIn().setBody("INSERT INTO Orders_log (company,description,quantity) "
+	           			+ "VALUES('" + order.getCustomer() + "','"
+	           					+ order.getItemDescription() +"','"
+	           					+ order.getItemQuantity() + "');");
 	            }
-	        });*/
+	        })
+	     .to("jdbc:ProcurementDB");
 	}
 }
--- src/main/java/org/group13/routingLogic/MainApp.java
+++ src/main/java/org/group13/routingLogic/MainApp.java
@@ -1,12 +1,17 @@
 package org.group13.routingLogic;
 
 import javax.jms.ConnectionFactory;
+import javax.sql.DataSource;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.SimpleRegistry;
 import org.apache.camel.main.Main;
+import org.apache.commons.dbcp.BasicDataSource;
+//import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * A Camel Application
@@ -17,13 +22,23 @@ public class MainApp {
      * A main() so we can easily run these routing rules in our IDE
      */
     public static void main(String... args) throws Exception {
-    	 CamelContext context = new DefaultCamelContext();
+    	 //CamelContext context = new DefaultCamelContext();
          
          // connect to embedded ActiveMQ JMS broker
-    	 ConnectionFactory connectionFactory = 
-             new ActiveMQConnectionFactory("vm://localhost");
-         context.addComponent("jms",
-             JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+ 		SimpleRegistry registry = new SimpleRegistry();
+
+ 		// code to create data source here
+ 		String url = "org.postgresql.Driver@localhost:5432:Procurement";
+ 		final DataSource ds = setupDataSource(url);
+ 		registry.put("ProcurementDB", ds);
+
+ 		CamelContext context = new DefaultCamelContext(registry);
+ 		initializeDatabase(ds);
+ 		
+ 		 ConnectionFactory connectionFactory =  new ActiveMQConnectionFactory("vm://localhost");
+ 	         context.addComponent("jms",
+ 	             JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+       	     
          context.addRoutes(new TransformationMapping());
          context.addRoutes(new OrderManagement());
          context.addRoutes(new InvoiceManagement());
@@ -34,5 +49,32 @@ public class MainApp {
          // stop the CamelContext
          context.stop();
     }
+    private static DataSource setupDataSource(String connectURI) {
+    	 BasicDataSource ds = new BasicDataSource();
+         ds.setDriverClassName("org.postgresql.Driver");
+         ds.setUrl("jdbc:postgresql://localhost:5432/Procurement");
+         ds.setUsername("postgres");
+         ds.setPassword("postgres");
+         return ds;
+    }
+    
+    private static void initializeDatabase(DataSource ds){
+    	JdbcTemplate jdbc = new JdbcTemplate(ds);
+ 		jdbc.execute("CREATE TABLE IF NOT EXISTS Orders_log "+
+				"("+
+				"ID SERIAL PRIMARY KEY, "+
+				"Time_inserted timestamp default current_timestamp, "+
+				"Company varchar(255) NOT NULL, "+
+				"Description varchar(255), "+
+				"Quantity varchar(255)"+
+			")");
+ 		jdbc.execute("CREATE TABLE IF NOT EXISTS Invoices_log "+
+				"("+
+				"ID SERIAL PRIMARY KEY, "+
+				"Time_inserted timestamp default current_timestamp, "+
+				"Company varchar(255) NOT NULL, "+
+				"Description varchar(255), "+
+				"Quantity varchar(255)"+
+			")");
+    }
 }
-
